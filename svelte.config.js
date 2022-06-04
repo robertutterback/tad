9b5bfbd38396c3733506b0368b52d875a3bb5c6f
Robert Utterback <robertu13@gmail.com>
2022-06-04 15:27:29 -0500

    Use adapter-node, i.e., polka

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
+    kit: {
+	adapter: adapter(),
+	methodOverride: {
+	    allowed: ['PATCH', 'DELETE']
 	}
+    }
 };
 
 export default config;
